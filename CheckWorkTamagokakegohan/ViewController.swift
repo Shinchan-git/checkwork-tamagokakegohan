2db777e7b874ce7ceeda774a13517aba26cdcd34
Shinchan-git <gyoshu.bowie@gmail.com>
2023-01-24 03:42:28 +0900

    Add: Basic app

--- CheckWorkTamagokakegohan/ViewController.swift
+++ CheckWorkTamagokakegohan/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var label: UILabel!
+    
+    var number: Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
+    @IBAction func plus() {
+        number += 1
+        label.text = String(number)
+    }
 
 }
 
