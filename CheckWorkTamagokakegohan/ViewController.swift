2e859c08d2deadcb8725893973e79b1fbd1f3c14
Shinchan-git <gyoshu.bowie@gmail.com>
2023-01-24 03:04:08 +0900

    Initial Commit

---
+++ CheckWorkTamagokakegohan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CheckWorkTamagokakegohan
+//
+//  Created by Owner on 2023/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
